5860e60a4dd3f445a9682015cb48f52cb4cdad90
Jefferson <jeffersondesenvolver@gmail.com>
2025-02-22 07:30:01 -0300

    retorno em Png melhorias na renderização

--- src/app/Returns/png-generator/png-generator.component.ts
+++ src/app/Returns/png-generator/png-generator.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import html2canvas from 'html2canvas';  // Importando a biblioteca html2canvas
+import { AfterViewInit, Component } from '@angular/core';
+import html2canvas from 'html2canvas';
 import { ModalService } from '../../services/modal.service';
 
 @Component({
@@ -7,34 +7,53 @@ import { ModalService } from '../../services/modal.service';
   templateUrl: './png-generator.component.html',
   styleUrls: ['./png-generator.component.css']
 })
-export class PngGeneratorComponent {
-  png: any;
-
+export class PngGeneratorComponent implements AfterViewInit {
   showModal = false;
 
-  constructor(private modalService: ModalService) { }
-
+  constructor(private modalService: ModalService) {}
 
   ngOnInit(): void {
-    // Escutando mudanças de estado no serviço de modal
     this.modalService.modalState$.subscribe(state => {
       if (state.modalType === 'png' && state.open) {
         this.showModal = true;
-        this.generatePNG();  // Gera o ZPL automaticamente quando o modal for aberto
+        setTimeout(() => this.generatePNG(), 200); // Pequeno delay para garantir que o canvas esteja pronto
       } else {
         this.showModal = false;
       }
     });
   }
 
+  ngAfterViewInit(): void {
+    this.setupCanvas(); // Configura o canvas após a view ser inicializada
+  }
+
+  setupCanvas(): void {
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    if (canvas) {
+      canvas.width = 800; // Dimensões fixas
+      canvas.height = 600;
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        ctx.fillStyle = 'white'; // Fundo branco
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.font = '20px Arial';
+        ctx.fillStyle = 'black';
+        ctx.fillText('Teste PNG', 50, 50); // Exemplo de texto
+      }
+    }
+  }
+
   generatePNG(): void {
     const canvasElement = document.getElementById('canvas') as HTMLCanvasElement;
-    if (canvasElement) {
-      html2canvas(canvasElement).then((canvas) => {
-        const imgURL = canvas.toDataURL('image/png');
-        this.downloadImage(imgURL);
-      });
+    if (!canvasElement) {
+      console.error('Canvas não encontrado!');
+      return;
     }
+
+    html2canvas(canvasElement, { scale: 2 }).then((canvas) => {
+      const imgURL = canvas.toDataURL('image/png');
+      this.downloadImage(imgURL);
+    });
   }
 
   downloadImage(imageUrl: string): void {
